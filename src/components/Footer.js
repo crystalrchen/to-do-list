4c51664d7a72b6989ac0c52b814b248c1af2de6a
crystalrchen <crystalrchen@users.noreply.github.com>
2021-01-14 00:06:59 -0800

    adding header and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import "./Footer.css"
+function Footer() {
+  return (
+    <footer>
+      &copy; crystal chen
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import "./Header.css"
+function Header() {
+  return (
+    <header>
+    <img src="https://i.imgur.com/F65LbXA.png"/>
+      <h1>to-do list</h1>
+    </header>
+  );
+}
+
+export default Header;
